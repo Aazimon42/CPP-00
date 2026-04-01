e6b0c9e95992d2e3d0e4b9c6c0c319ebcaa83b2f
Enzo Di Maiolo <edi-maio@1D3.42angouleme.fr>
2026-04-01 19:29:45 +0200

    initial commit

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: edi-maio <edi-maio@42angouleme.fr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/17 17:58:01 by edi-maio          #+#    #+#             */
+/*   Updated: 2026/03/28 17:40:04 by edi-maio         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main(int ac, char **av)
+{
+	int	i;
+	int	j;
+
+	if (ac < 2)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (1);
+	}
+	i = 1;
+	while (av[i])
+	{
+		j = 0;
+		while (av[i][j])
+			std::cout << (char)toupper(av[i][j++]) << std::endl;
+		i++;
+	}
+}
