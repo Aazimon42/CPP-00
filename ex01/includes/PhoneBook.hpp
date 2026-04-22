f7d080a9724bd404e11647da856c0077d7250737
Enzo Di Maiolo <edi-maio@3F1.42angouleme.fr>
2026-04-22 15:12:08 +0200

    clean

--- ex01/includes/PhoneBook.hpp
+++ ex01/includes/PhoneBook.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   PhoneBook.hpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: edi-maio <edi-maio@42angouleme.fr>         +#+  +:+       +#+        */
+/*   By: edi-maio <edi-maio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/17 18:58:08 by edi-maio          #+#    #+#             */
-/*   Updated: 2026/04/01 19:33:22 by edi-maio         ###   ########.fr       */
+/*   Updated: 2026/04/22 15:06:11 by edi-maio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,13 @@
 # include "Contact.hpp"
 
 class PhoneBook{
-public:
-	Contact contacts[8];
-	int		index;
-	void	add(void);
-	void	search(void);
-	PhoneBook (void);
+	private:
+		Contact contacts[8];
+		int		index;
+	public:
+		PhoneBook (void);
+		void	add(void);
+		void	search(void);
 };
 
 #endif
